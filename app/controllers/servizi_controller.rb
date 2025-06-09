fdfbc0b1491f50c5c550e8ef370fcdb14f969082
Giuseppe Rezzi <g.rezzi@donboscosandona.it>
2025-06-09 17:26:03 +0200

    andato avanti con l'esercizio

---
+++ app/controllers/servizi_controller.rb
@@ -0,0 +1,4 @@
+class ServiziController < ApplicationController
+    def servizi 
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get '/home', to: 'home#index'
+  get '/servizi', to: 'home#servizi'
   root 'home#index'
 end
