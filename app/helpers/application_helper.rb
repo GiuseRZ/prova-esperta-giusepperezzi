1fc57a0dd8b531bc663fc9d4158c71246f3ae4a6
Giuseppe Rezzi <g.rezzi@donboscosandona.it>
2025-06-09 16:12:02 +0200

    metà

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
-end
+    def site_title
+      "Esperta Academy"
+    end
+  
+    def nav_links
+      {
+        "Home" => root_path,
+        "Corsi" => "#",
+        "Chi siamo" => "#",
+        "Contatti" => "#"
+      }
+    end
+  end
+  
